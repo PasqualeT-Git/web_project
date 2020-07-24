c9ba0251b2bfd6f8dacc6cbcdac9b8b73af14d1a
Pasquale Troise <troisepasquale1990@gmail.com>
2020-07-24 17:01:51 +0100

    Implemented devise and added a simple form for add customers in contact page

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,24 @@
+class CustomersController < ApplicationController
+
+  def new
+    @customer = Customer.new
+  end
+  
+ 
+  def create
+    @customer = Customer.new(packets_params)
+
+    if @customer.save
+      redirect_to about_path
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def packets_params
+    params.require(:customer).permit(:name, :e_mail, :phone_number, :message)    
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'portfolio', to: 'pages#portfolio'
   get 'product', to: 'pages#product'
+  post 'new_customer', to: 'pages#new_customer'
+  
+  resources :customers
 end
