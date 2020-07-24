b9512c4678dd9549fa15d26aa7f26209971b3c2b
Pasquale Troise <troisepasquale1990@gmail.com>
2020-07-24 11:11:49 +0100

    Fixed the issue of 'refresh-page-needed' for the typed effect in typed.js

--- app/javascript/components/type_effect.js
+++ app/javascript/components/type_effect.js
@@ -1,19 +1,20 @@
 import Typed from 'typed.js';
 
-const element = document.getElementById('typing')
 
 const initTypedJs = () => {
- if (element) {
-   new Typed('#typing', {
-    strings: ['creating','designing','testing','planning','developing','selecting'],
-    typeSpeed: 60,
-    backSpeed: 100,
-    showCursor: true,
-    smartBackspace: true,
-    loop: true,
-    loopCount: Infinity
-    });
-  }
+  const element = document.getElementById('typing')
+
+  if (element) {
+    new Typed('#typing', {
+      strings: ['creating','designing','testing','planning','developing','selecting'],
+      typeSpeed: 60,
+      backSpeed: 100,
+      showCursor: true,
+      smartBackspace: true,
+      loop: true,
+      loopCount: Infinity
+      });
+    }
 };
 
 export { initTypedJs };
