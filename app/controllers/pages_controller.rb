dcba8f1237bb8716b1123fdb9d9c22321f3cc86f
Pasquale Troise <troisepasquale1990@gmail.com>
2020-07-27 09:00:27 +0100

    Added an andministration page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, :raise => false
   before_action :authenticate_user!, only: [:admin]
   
-  # USER = User.find(params[:id])
 
   def home
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/administrator', as: 'rails_admin'
   devise_for :users
 
   root to: 'pages#home'
@@ -10,5 +11,5 @@ Rails.application.routes.draw do
   get 'admin', to: 'pages#admin'
   post 'new_customer', to: 'pages#new_customer'
   
-  resources :customers
+  resources :customers, only: [:new, :create]
 end
