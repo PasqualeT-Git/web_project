884eb4ee6a0aa81d187b1d5579ec0b6b27282f3a
Pasquale Troise <troisepasquale1990@gmail.com>
2020-07-27 11:47:35 +0100

    Added a class to the labels of simple_form

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/administrator', as: 'rails_admin'
   devise_for :users
  
- 
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
